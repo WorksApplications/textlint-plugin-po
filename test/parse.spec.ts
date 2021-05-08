117cb2563bf6bdbc8c6cf383e1e191531f92a9ad
Kengo TODA <skypencil+github@gmail.com>
2021-05-08 10:12:18 +0800

    test: extract expected result to a json file

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -5,6 +5,7 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { test } from '@textlint/ast-tester';
 import singlelineJson from './singleline.json';
+import multilineJson from './multiline.json';
 
 describe('#parse', () => {
     describe('when parsing .po file with single line msgstr', () => {
@@ -22,57 +23,7 @@ describe('#parse', () => {
         it('should return an expected AST', () => {
             const result = parse(po);
             test(result);
-            expect(result).to.eql({
-                type: 'Document',
-                raw: po,
-                range: [0, 743],
-                loc: {
-                    start: {
-                        line: 1,
-                        column: 1,
-                    },
-                    end: {
-                        line: 32,
-                        column: 6,
-                    },
-                },
-                children: [
-                    {
-                        type: 'Str',
-                        raw: 'msgstr ""\n"複数行にまたがる"\n"事例"',
-                        value: '複数行にまたがる事例',
-                        range: [605, 632],
-                        loc: {
-                            start: {
-                                line: 22,
-                                column: 1,
-                            },
-                            end: {
-                                line: 24,
-                                column: 4,
-                            },
-                        },
-                        children: [],
-                    },
-                    {
-                        type: 'Str',
-                        raw: `msgstr \"\"\n\"複数行にまたがる\"\n\"他の事例"`,
-                        value: '複数行にまたがる他の事例',
-                        range: [713, 742],
-                        loc: {
-                            start: {
-                                line: 30,
-                                column: 1,
-                            },
-                            end: {
-                                line: 32,
-                                column: 6,
-                            },
-                        },
-                        children: [],
-                    },
-                ],
-            });
+            expect(result).to.eql(multilineJson);
         });
     });
 });
